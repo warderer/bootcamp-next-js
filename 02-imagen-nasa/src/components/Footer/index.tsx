39d7e576e3273750caa1875546066cbbe3d607d7
César Guerra <warderer@gmail.com>
2023-04-11 17:41:41 -0600

    ✨ Agregando Layouts

---
+++ 02-imagen-nasa/src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Proyecto Imagen de la Nasa</p>
+    </footer>
+  )
+}
+export default Footer
---
+++ 02-imagen-nasa/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Bienvenido a Imagenes de la Nasa</h1>
+    </header>
+  )
+}
+export default Header
