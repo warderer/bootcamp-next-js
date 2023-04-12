5a66de9dfe5212e65440c70d2839946fbff3d50a
César Guerra <warderer@gmail.com>
2023-04-12 00:12:25 -0600

    ✨ CSS Modules & next/Image

--- 02-imagen-nasa/src/components/ImageOfTheDay/index.tsx
+++ 02-imagen-nasa/src/components/ImageOfTheDay/index.tsx
@@ -1,10 +1,20 @@
-import { Image } from '@/types'
+import { Image as ImageTypes } from '@/types'
+import Image from 'next/image'
 import Router from 'next/router'
+import styles from './ImageOfTheDay.module.css'
 
-const ImageOfTheDay = ({ url, title, date }: Image) => {
+const ImageOfTheDay = ({ url, title, date }: ImageTypes) => {
   return (
-    <div>
-      <img src={url} alt={title} onClick={() => Router.push(`image/${date}`)} />
+    <div className={styles.container}>
+      <Image
+        src={url || ''}
+        alt={title || ''}
+        onClick={() => Router.push(`image/${date}`)}
+        // width={200}
+        // height={200}
+        fill
+        className={styles.image}
+      />
       <h2>{title}</h2>
     </div>
   )
