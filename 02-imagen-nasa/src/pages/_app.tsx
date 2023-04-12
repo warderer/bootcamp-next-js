c387df9155271c07accf46645befbb190212fb28
César Guerra <warderer@gmail.com>
2023-04-12 00:13:18 -0600

    💄 Google fonts

--- 02-imagen-nasa/src/pages/_app.tsx
+++ 02-imagen-nasa/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['100', '300', '500'],
+  subsets: ['latin'],
+  preload: true
+})
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={roboto.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   )
 }
